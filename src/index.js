ddf79d9d4f2f9f15ff3994d81d4a2df2686eef8b
Michael Piantella <piantella.michael@gmail.com>
2026-01-26 14:18:58 +0100

    Revert "first commit"
    This reverts commit 932c40293ccb11f783759d9e16e24b3f5552613b.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-// Legacy entrypoint from the original boilerplate.
-// The web component is now defined in `src/main.js` and webpack points there.
-// Keeping this file avoids confusion if someone still looks for boilerplate code.
-
-export {};
